1915a5426534b1d9df2bb67fbc9781626a299113
Pranav Gokhale <pranav@super.tech>
2022-01-13 10:27:33 -0600

    Update qiskit_superstaq/superstaq_provider.py
    Co-authored-by: richrines1 <85512171+richrines1@users.noreply.github.com>

--- qiskit_superstaq/superstaq_provider.py
+++ qiskit_superstaq/superstaq_provider.py
@@ -171,7 +171,7 @@ class SuperstaQProvider(
         self,
         circuits: Union[qiskit.QuantumCircuit, List[qiskit.QuantumCircuit]],
         target: str = "ibmq_qasm_simulator",
-    ) -> qiskit.pulse.Schedule:
+    ) -> Union[qiskit.pulse.Schedule, List[qiskit.pulse.Schedule]]:
         """Returns pulse schedule(s) for the given circuit(s) and target."""
         serialized_circuits = qss.serialization.serialize_circuits(circuits)
 
